c8401b36b72220894319c8afb509c5c9ef7f984e
joshyrout <joshyrout@gmail.com>
2021-01-20 13:26:32 -0700

    Books assignment complete

--- src/books/Book.java
+++ src/books/Book.java
@@ -1,4 +1,98 @@
 package books;
 
-public class Book {
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Book implements Comparable<Book> {
+
+    private String title;
+    private String author;
+    private int year;
+
+    public Book(String title, String author, int year){
+        this.title = title;
+        this.author = author;
+        this.year = year;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    @Override
+    public String toString(){
+        // return string of "name by author (year)"
+        String asString = title + " by " + author + " (" + year + ")";
+        return asString;
+    }
+
+    public static List<Book> getList(String file){
+        List list = new ArrayList<Book>();
+        String line = "";
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(file));
+            while((line = br.readLine()) != null){
+                String[] bookCSV = line.split(",");
+                if (inValid(line)){
+                    String name = bookCSV[0];
+                    String author = bookCSV[1];
+                    int year = Integer.parseInt(bookCSV[2]);
+                    list.add(new Book(name, author, year));
+
+                }
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return list;
+    }
+
+    private static boolean isInt(String testString){
+        try{
+            int isInt = Integer.parseInt(testString);
+            return true;
+        } catch(Exception e) {
+            return false;
+        }
+    }
+     private static boolean inValid(String line){
+
+        String[] testArray = line.split(",");
+        if(testArray.length == 3) {
+            if (!testArray[0].isEmpty() && !testArray[1].isEmpty() && !testArray[2].isEmpty()) {
+                try {
+                    int isInt = Integer.parseInt(testArray[2]);
+                    return true;
+                } catch (Exception e) {
+                    System.err.println("Unable to read: \"" + line + "\" The year was not an integer.");
+                    return false;
+                }
+            } else {
+                System.err.println("Unable to read: \"" + line + "\" One or more of the entries are empty");
+                return false;
+            }
+        } else {
+            System.err.println("Unable to read: \"" + line + "\" Invalid number of entries in line");
+            return false;
+        }
+     }
+
+    @Override
+    public int compareTo(Book o) {
+        return this.title.compareTo(o.title);
+    }
 }
--- src/books/BookApp.java
+++ src/books/BookApp.java
@@ -1,4 +1,47 @@
 package books;
 
+import edu.princeton.cs.algs4.Selection;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 public class BookApp {
+
+    public static void main(String[] args) {
+
+        Book[] bookArray = getBookArray("src/books/books.csv");
+        sortNatural(bookArray);
+        sortReverse(bookArray);
+
+    }
+
+    private static void sortNatural(Book[] bookArray){
+        System.out.println("\nBooks sorted in natural order:");
+        Arrays.sort(bookArray);
+        for(int i = 0; i < bookArray.length; i++){
+            System.out.println(bookArray[i].toString());
+        }
+    }
+
+    private static void sortReverse(Book[] bookArray){
+        System.out.println("\nBooks sorted in reverse order:");
+        Arrays.sort(bookArray, Collections.reverseOrder());
+        for(int i = 0; i < bookArray.length; i++){
+            System.out.println(bookArray[i].toString());
+        }
+    }
+
+    private static Book[] getBookArray(String file){
+        List<Book> listOfBooks = Book.getList(file);
+        System.out.println("Number of books in file:" +listOfBooks.size());
+        Book[] bookArray = new Book[listOfBooks.size()];
+        for(int i = 0; i < listOfBooks.size(); i++){
+            bookArray[i] = listOfBooks.get(i);
+        }
+        return bookArray;
+    }
+
+
+
 }
