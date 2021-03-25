09a4e123fc254183a4ffd9e1991be16a55c16014
Joshua Routledge <joshyrout@gmail.com>
2021-03-24 20:20:36 -0600

    a04 part 2 started

--- src/a04/Board.java
+++ src/a04/Board.java
@@ -2,40 +2,37 @@ package a04;
 
 import edu.princeton.cs.algs4.Stack;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Board {
 
     //use a heap to track the priority of each board. the priority is the Hamming number(future cost estimate) + the number of moves it has already taken(distance from start).
     //then check the lowest priority board in the heap
     //use a* algorithm as it always find the shortest path as long as the future cost estimate is correct
     //build a private class called SearchNode that contains a board, the moves it has made, its priority and the previous board
-    /* -- isSolvable()
-        you can tell if a board is solvable with a parody based argument. like in chess a white bishop can never end up on black and vice versa
-        on an odd board if the inversion is odd then it is unsolvable
-        on an even board you add the row number containing the blank space to the number of inversions and if its even it is unsolvable
-        on even the sum always changes by an even amount
-     */
-    //don't cast an iterable to whatever you use. ie don't do this Stack<Board> a = (Stack<Board>) x.neighbors();
-    //follow the rules in the book for writing an equals method
-
-    // construct a board from an N-by-N array of blocks
-    // (where blocks[i][j] = block in row i, column j)
 
     private final int[][] boardArray;
     private final int N;
-    private int indexOf0;
+    private int zeroRow;
+    private int zeroCol;
 
     public Board(int[][] blocks) {
         this.N = blocks.length;
-        this.boardArray = blocks;
         int count = 0;
+
+        int[][] tempArray = new int[N][N];
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < N; j++) {
-                if(boardArray[i][j] == 0){
-                    this.indexOf0 = count;
+                tempArray[i][j] = blocks[i][j];
+                if(tempArray[i][j] == 0){
+                    zeroRow = i;
+                    zeroCol = j;
                 }
                 count++;
             }
         }
+        this.boardArray = tempArray;
     }
 
     // board size N
@@ -111,7 +108,7 @@ public class Board {
             }
         }
         if ((N % 2) == 0) {
-            inversions += (indexOf0 % N);
+            inversions += zeroRow;
             if ((inversions % 2) == 0) {
                 return false;
             } else {
@@ -128,69 +125,52 @@ public class Board {
 
     // does this board equal y?
     public boolean equals(Object y) {
+        if (y == this) return true;
+        if (y == null) return false;
+        if (this.getClass() != y.getClass()) return false;
 
-        if(y == null) return false;
-        if(this.getClass() != y.getClass()) return false;
         Board other = (Board) y;
-        if(this.boardArray != other.boardArray) return false;
+        for (int i = 0; i < N ; i++)
+            for (int j = 0; j < N ; j++)
+                if (this.boardArray[i][j] != other.boardArray[i][j]) return false;
+
         return true;
+
     }
 
     // all neighboring boards
     public Iterable<Board> neighbors() {
         Stack<Board> stack = new Stack<Board>();
-        int row = (indexOf0 / N);
-        int column = (indexOf0 % N);
-        if (row != 0) {
-            int[][] neighborArray = new int[N][N];
-            for (int i = 0; i < N; i++) {
-                for (int j = 0; j < N; j++) {
-                    neighborArray[i][j] = boardArray[i][j];
-                }
-            }
 
-            neighborArray[row][column] = neighborArray[row - 1][column];
-            neighborArray[row - 1][column] = 0;
-            Board board = new Board(neighborArray);
+        if (zeroCol != 0) {
+            Board board = new Board(swap(zeroRow, zeroCol - 1));
             stack.push(board);
         }
-        if (row != (N - 1)) {
-            int[][] neighborArray = new int[N][N];
-            for (int i = 0; i < N; i++) {
-                for (int j = 0; j < N; j++) {
-                    neighborArray[i][j] = boardArray[i][j];
-                }
-            }
-            neighborArray[row][column] = neighborArray[row + 1][column];
-            neighborArray[row + 1][column] = 0;
-            Board board = new Board(neighborArray);
+        if (zeroRow != 0) {
+            Board board = new Board(swap(zeroRow - 1, zeroCol));
             stack.push(board);
         }
-        if (column != 0) {
-            int[][] neighborArray = new int[N][N];
-            for (int i = 0; i < N; i++) {
-                for (int j = 0; j < N; j++) {
-                    neighborArray[i][j] = boardArray[i][j];
-                }
-            }
-            neighborArray[row][column] = neighborArray[row][column - 1];
-            neighborArray[row][column - 1] = 0;
-            Board board = new Board(neighborArray);
+        if (zeroRow != (N - 1)) {
+            Board board = new Board(swap(zeroRow + 1, zeroCol));
             stack.push(board);
         }
-        if (column != (N - 1)) {
-            int[][] neighborArray = new int[N][N];
-            for (int i = 0; i < N; i++) {
-                for (int j = 0; j < N; j++) {
-                    neighborArray[i][j] = boardArray[i][j];
-                }
-            }
-            neighborArray[row][column] = neighborArray[row][column + 1];
-            neighborArray[row][column + 1] = 0;
-            Board board = new Board(neighborArray);
+        if (zeroCol != (N - 1)) {
+            Board board = new Board(swap(zeroRow, zeroCol + 1));
             stack.push(board);
         }
         return stack;
+    }
+
+    private int[][] swap(int row, int col){
+        int[][] neighborArray = new int[N][N];
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                neighborArray[i][j] = boardArray[i][j];
+            }
+        }
+        neighborArray[zeroRow][zeroCol] = neighborArray[row][col];
+        neighborArray[row][col] = 0;
+        return neighborArray;
     }
 
     // string representation of this board (in the output format specified below)
@@ -208,6 +188,7 @@ public class Board {
 
 
 
+
     // unit tests (not graded)
     public static void main(String[] args) {
 
@@ -224,198 +205,5 @@ public class Board {
         for(Board board : neighbor){
             System.out.println(board);
         }
-
-        System.out.println(initialBoard);
-
     }
 }
-    /*
-    private int[][] boardArray;
-    private int N;
-//    private int indexOf0;
-
-    public Board(int[][] blocks) {
-        N = blocks.length;
-        boardArray = blocks;
-//        int count = 0;
-//        for (int i = 0; i < N; i++) {
-//            for (int j = 0; j < N; j++) {
-//                boardArray[i][j] = blocks[i][j];
-//                if(boardArray[i][j] == 0){
-//                    indexOf0 = count;
-//                    return;
-//                }
-//                count++;
-//            }
-//        }
-    }
-
-    // board size N
-    public int size() {
-        return N;
-    }
-
-    // number of blocks out of place
-    public int hamming() {
-        int count = 0;
-        int index = 0;
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < N; j++) {
-                index++;
-                if (boardArray[i][j] != 0) {
-                    if ((index) != boardArray[i][j]) count++;
-                }
-            }
-        }
-        return count;
-    }
-
-    // sum of Manhattan distances between blocks and goal
-    public int manhattan() {
-        int count = 0;
-        int index = 0;
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < N; j++) {
-                index++;
-                if (boardArray[i][j] != 0) {
-                    int compare = Math.abs((index) - boardArray[i][j]);
-                    if (compare != 0) {
-                        while (compare >= N) {
-                            compare = compare - N;
-                            count++;
-                        }
-                        while (compare >= 1) {
-                            compare = compare - 1;
-                            count++;
-                        }
-                    }
-                }
-            }
-        }
-        return count;
-    }
-
-    // is this board the goal board?
-    public boolean isGoal() {
-        return hamming() == 0;
-    }
-
-    // is this board solvable?
-    public boolean isSolvable() {
-        int inversions = 0;
-        int primaryIndex = 0;
-        int rowOf0 = 0;
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < N; j++) {
-                primaryIndex++;
-                if (boardArray[i][j] != 0) {
-                    int comparedIndex = 0;
-                    for (int k = 0; k < boardArray.length; k++) {
-                        for (int l = 0; l < N; l++) {
-                            comparedIndex++;
-                            if (boardArray[i][j] != 0) {
-                                if (boardArray[i][j] < boardArray[k][l] && comparedIndex < primaryIndex) {
-                                    inversions++;
-                                }
-                            }
-                        }
-                    }
-                } else {
-                    rowOf0 = i;;
-                }
-            }
-        }
-        if ((N % 2) == 0) {
-            inversions += rowOf0;
-            if ((inversions % 2) == 0) {
-                return false;
-            } else {
-                return true;
-            }
-        } else {
-            if ((inversions % 2) == 0) {
-                return true;
-            } else {
-                return false;
-            }
-        }
-    }
-
-    // does this board equal y?
-    public boolean equals(Object y) {
-        return false;
-    }
-
-    // all neighboring boards
-    public Iterable<Board> neighbors() {
-        Queue<Board> queue = new Queue<Board>();
-        for(){
-
-        }
-        int row = (indexOf0 / N);
-        int column = (indexOf0 % N);
-        if (row != 0) {
-            int[][] neighborArray = boardArray;
-            neighborArray[row][column] = neighborArray[row - 1][column];
-            neighborArray[row - 1][column] = 0;
-            Board board = new Board(neighborArray);
-            queue.enqueue(board);
-        }
-        if (row != (N - 1)) {
-            int[][] neighborArray = boardArray;
-            neighborArray[row][column] = neighborArray[row + 1][column];
-            neighborArray[row + 1][column] = 0;
-            Board board = new Board(neighborArray);
-            queue.enqueue(board);
-        }
-        if (column != 0) {
-            int[][] neighborArray = boardArray;
-            neighborArray[row][column] = neighborArray[row][column - 1];
-            neighborArray[row][column - 1] = 0;
-            Board board = new Board(neighborArray);
-            queue.enqueue(board);
-        }
-        if (column != (N - 1)) {
-            int[][] neighborArray = boardArray;
-            neighborArray[row][column] = neighborArray[row][column + 1];
-            neighborArray[row][column + 1] = 0;
-            Board board = new Board(neighborArray);
-            queue.enqueue(board);
-        }
-        return queue;
-    }
-
-    // string representation of this board (in the output format specified below)
-    public String toString() {
-        StringBuilder sb = new StringBuilder();
-        sb.append(N + "\n");
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < N; j++) {
-                sb.append(String.format("%2d", boardArray[i][j]));
-            }
-            sb.append("\n");
-        }
-        return sb.toString();
-    }
-
-    // unit tests (not graded)
-    public static void main(String[] args) {
-
-        int[][] inputArray = {{1, 2, 3}, {4, 5, 6}, {7, 8, 0}};
-        Board initialBoard = new Board(inputArray);
-        System.out.println(initialBoard.hamming());
-        System.out.println(initialBoard.manhattan());
-        System.out.println(initialBoard.isGoal());
-        System.out.println(initialBoard.isSolvable());
-        System.out.println(initialBoard);
-
-        Iterable<Board> neighbor = initialBoard.neighbors();
-
-        for(Board board : neighbor){
-            System.out.println(board);
-        }
-
-
-    }
-     */
-
---
+++ src/ceStock/StockApp.java
@@ -0,0 +1,111 @@
+package ceStock;
+
+import edu.princeton.cs.algs4.ST;
+
+import javax.swing.text.DateFormatter;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class StockApp {
+
+    private static ST<Date, Double> readCSV(String fileName, int keyField, int valField){
+        ST<Date, Double> st = new ST<Date, Double>();
+        try {
+            File file = new File("C:\\Users\\Josh\\IdeaProjects\\2420_GettingStarted\\resources\\" + fileName);
+            if (file.exists()) {
+                BufferedReader br;
+                String line;
+                String csvSplitBy = ",";
+                br = new BufferedReader(new FileReader(file));
+                br.readLine();
+                while ((line = br.readLine()) != null) {
+                    String[] currentLine = line.split(csvSplitBy);
+                    try{
+//                        String dateString = currentLine[keyField];
+//                        SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
+                        java.util.Date date = stringToDate(currentLine[keyField], "yyyy-MM-dd");
+                        Double valDouble = Double.parseDouble(currentLine[valField]);
+                        st.put(date, valDouble);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                        continue;
+                    }
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return st;
+    }
+
+    public static void main(String[] args) {
+        ST<Date, Double> st = readCSV("AMZN.csv", 0, 4);
+        System.out.println("Number of elements: " + st.size() + "\n");
+
+        // A-C
+        Date key1 = st.min();
+        double d1 = st.get(key1);
+        print("A) Oldest closing price: ", key1, "%tF", d1);
+
+        Date key2 = st.max();
+        double d2 = st.get(key2);
+        print("B) Newest closing price: ", key2, "%tF", d2);
+
+        difference("C) The closing price ", d1, d2);
+
+        //D-F
+        key1 = stringToDate("08/31/18", "MM/dd/yy");
+        d1 = st.get(key1);
+        print("D) Closing price from ", key1, "%tm/%<td/%<tY", d1);
+
+        key2 = stringToDate("03/10/20", "MM/dd/yy");
+        d2 = st.get(key2);
+        print("E) Closing price from ", key2, "%tm/%<td/%<tY", d2);
+
+        difference("F) The closing price ", d1, d2);
+
+        //G & H
+        key1 = st.ceiling(stringToDate("01/01/19", "MM/dd/yy"));
+        d1 = st.get(key1);
+        print("G) first available date in 2019: ", key1, "%tm/%<td/%<tY", d1);
+
+        key1 = st.floor(stringToDate("01/01/21", "MM/dd/yy"));
+        d1 = st.get(key1);
+        print("H) first available date in 2020: ", key1, "%tm/%<td/%<tY", d1);
+
+    }
+
+    private static void print(String print, Date date, String dateFormat, double dollars){
+        StringBuilder sb = new StringBuilder();
+        sb.append(print);
+        sb.append(String.format(dateFormat, date));
+        sb.append(String.format(" $%.2f.", dollars));
+        System.out.println(sb.toString());
+    }
+
+    private static void difference(String print, double d1, double d2){
+        StringBuilder sb = new StringBuilder();
+        sb.append(print);
+        double difference = d2 - d1;
+
+        if(difference >= 0) sb.append("increased by");
+        else                sb.append("decreased by");
+        sb.append(String.format(" $%.2f.", Math.abs(difference)));
+        System.out.println(sb.toString());
+    }
+
+    private static Date stringToDate(String dateString, String pattern){
+        try{
+            SimpleDateFormat formatter = new SimpleDateFormat(pattern);
+            java.util.Date date = formatter.parse(dateString);
+            return date;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
