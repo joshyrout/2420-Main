e87636e43a9814d91579c3f8ac1c99ae5525fd39
Joshua Routledge <joshyrout@gmail.com>
2021-04-07 21:45:35 -0600

    a05 part 1 complete

--- src/a05/KdTreeST.java
+++ src/a05/KdTreeST.java
@@ -3,6 +3,7 @@ package a05;
 
 import edu.princeton.cs.algs4.*;
 import java.math.BigDecimal;
+import java.security.cert.PolicyNode;
 
 public class KdTreeST<Value> {
     private Node root;
@@ -31,9 +32,8 @@ public class KdTreeST<Value> {
         }
 
         public int compareTo(Point2D p) {
-            double compare1;
-            double compare2;
-            int compareResult;
+            Double compare1;
+            Double compare2;
 
             if(this.layer % 2 == 0){
                 compare1 = this.p.x();
@@ -43,36 +43,10 @@ public class KdTreeST<Value> {
                 compare2 = p.y();
             }
 
-            if(compare1 > compare2)compareResult = (int) - (Math.ceil(compare1 - compare2));
-            else                   compareResult = (int) Math.ceil(compare2 - compare1);
-
-            return compareResult;
-        }
-
-        public int trueCompare(Point2D p){
-            int compare1;
-            int compare2;
-
-            BigDecimal bd1;
-            BigDecimal bd2;
-            if(this.layer % 2 == 0){
-                bd1 = BigDecimal.valueOf(this.p.x());
-                bd2 = BigDecimal.valueOf(p.x());
-            } else {
-                bd1 = BigDecimal.valueOf(this.p.y());
-                bd2 = BigDecimal.valueOf(p.y());
-            }
-
-            bd1 = bd1.movePointRight(6);
-            bd2 = bd2.movePointRight(6);
-
-            compare1 = bd1.intValue();
-            compare2 = bd2.intValue();
-            return (compare1 - compare2);
+            return Double.compare(compare2, compare1);
         }
 
         public double distance(Point2D p){
-            //TODO : this is where i was
             double compare1;
             double compare2;
 
@@ -84,7 +58,24 @@ public class KdTreeST<Value> {
                 compare2 = p.y();
             }
 
-            return (compare1 - compare2);
+            return compare2 - compare1;
+        }
+
+        public double distanceBD(Point2D p){ //TODO: remove
+            BigDecimal compare1;
+            BigDecimal compare2;
+
+            if(this.layer % 2 == 0){
+                compare1 = BigDecimal.valueOf(this.p.x());
+                compare2 = BigDecimal.valueOf(p.x());
+            } else {
+                compare1 = BigDecimal.valueOf(this.p.y());
+                compare2 = BigDecimal.valueOf(p.y());
+            }
+
+            compare1 = compare2.subtract(compare1);
+
+            return compare1.doubleValue();
         }
 
         public void updateLayer(){
@@ -99,10 +90,10 @@ public class KdTreeST<Value> {
             double maxY = this.rect.ymax();
 
             if (this.layer % 2 != 0){
-                if (compare < 0) maxX = p.x();
+                if (compare > 0) maxX = p.x();
                 else             minX = p.x();
             } else {
-                if (compare < 0) maxY = p.y();
+                if (compare > 0) maxY = p.y();
                 else             minY = p.y();
             }
             this.rect = new RectHV(minX, minY, maxX, maxY);
@@ -116,7 +107,7 @@ public class KdTreeST<Value> {
 
     // is the symbol table empty?
     public boolean isEmpty(){
-        return size != 0;
+        return (root == null);
     }
 
     // number of points
@@ -134,25 +125,25 @@ public class KdTreeST<Value> {
     public void put(Point2D p, Value val){
         if(p == null || val == null) throw new NullPointerException("inputs can't be null");
         RectHV rect = new RectHV(Double.NEGATIVE_INFINITY,Double.NEGATIVE_INFINITY,Double.POSITIVE_INFINITY,Double.POSITIVE_INFINITY);
-        Node child = new Node(p, val, rect, 0, 1);
+        Node child = new Node(p, val, rect, 1, 1);
         root = put(root, child);
     }
 
     private Node put(Node parent, Node child){
         if (parent == null) {
-//            StdOut.println("child created: " + child.p + "  layer: "+ child.layer + "  rect: " + child.rect);
+//            StdOut.println("child created: " + child.p + "  layer: "+ child.layer + "  rect: " + child.rect); TODO: remove
             return child;
         }
-        int compare = parent.compareTo(child);
-//        System.out.println("Parent: " + parent.p + "  Child: " + child.p + "  Compare: " + compare + "  Layer: " + parent.layer);
+        int compare = child.compareTo(parent);
+//        System.out.println("Parent: " + parent.p + "  Child: " + child.p + "  Compare: " + compare + "  Layer: " + parent.layer); TODO: remove
         child.updateLayer();
         child.updateRect(compare, parent.p);
         if (compare < 0) parent.lb  = put(parent.lb,  child);
-        else             parent.rt = put(parent.rt, child);
+        else if (compare > 0) parent.rt = put(parent.rt, child);
+        else if(child.p.compareTo(parent.p) == 0) parent.value = child.value;
         parent.size = size(parent.lb) + size(parent.rt) + 1;
         return parent;
     }
-
 
     // TODO: value associated with point p
     public Value get(Point2D p){
@@ -162,19 +153,17 @@ public class KdTreeST<Value> {
 
     private Value get(Node parent, Point2D p) {
         if (parent == null) return null;
-        int compare = p.compareTo(parent.p);
-        if(compare == 0) return parent.value;
-        compare = parent.compareTo(p);
+        int compare = parent.p.compareTo(p);
         if      (compare < 0) return get(parent.lb, p);
-        else                  return get(parent.rt, p);
+        else if (compare < 0) return get(parent.rt, p);
+        else return parent.value;
     }
 
     // does the symbol table contain point p?
     public boolean contains(Point2D p){
         if(p == null) throw new NullPointerException("input can't be null");
-        // check if the point is in a nodes rectangle if it is continue and check the point and its subtrees if not stop.
         Value value = get(p);
-        return (value == null);
+        return (value != null);
     }
 
     // TODO: all points in the symbol table
@@ -206,7 +195,7 @@ public class KdTreeST<Value> {
         range(parent.rt, queue, rect);
     }
 
-    // TODO: a nearest neighbor to point p; null if the symbol table is empty
+    // a nearest neighbor to point p; null if the symbol table is empty
     public Point2D nearest(Point2D p){
         if(p == null) throw new NullPointerException("input can't be null");
         if(isEmpty()) return null;
@@ -218,8 +207,8 @@ public class KdTreeST<Value> {
         if(nearest == null) throw new NullPointerException("The nodes cant be null");
         if(node == null) return nearest;
         double currentDistance = nearest.distance(p);
-        System.out.println("current distance: " + Math.abs(currentDistance) + " node distance: " + Math.abs(node.distance(p)));
-        if(Math.abs(node.distance(p)) > Math.abs(currentDistance)) return nearest;
+//        System.out.println("current distance: " + Math.abs(currentDistance) + " node distance: " + Math.abs(node.distance(p))); //TODO: remove
+        if(!node.rect.contains(p) && Math.abs(node.distance(p)) > Math.abs(currentDistance)) return nearest;
         if(Math.abs(node.p.distanceSquaredTo(p)) < Math.abs(nearest.p.distanceSquaredTo(p))) nearest = node;
 
         if(currentDistance >= 0){
@@ -229,19 +218,19 @@ public class KdTreeST<Value> {
             nearest = nearest(node.lb, p, nearest);
             nearest = nearest(node.rt, p, nearest);
         }
-
         return nearest;
     }
 
 
     // unit testing of the methods (not graded)
     public static void main(String[] args){
-        String filename = "resources/a05/input1K.txt";
+        String filename = "resources/a05/input100k.txt";
         In in = new In(filename);
 //        RectHV rect = new RectHV(0.25, 0.25, 0.75, 0.75);
         RectHV rect = new RectHV(0.75, 0.75, 1, 1);
 
         KdTreeST<Integer> kdTree = new KdTreeST<Integer>();
+        System.out.println("Is kdTree empty: " + kdTree.isEmpty());
         for (int i = 0; !in.isEmpty(); i++) {
             double x = in.readDouble();
             double y = in.readDouble();
@@ -257,6 +246,17 @@ public class KdTreeST<Value> {
             count ++;
         }
         System.out.println("Number of points: " + count);
+
+        count = 0;
+        in = new In(filename);
+        for (int i = 0; !in.isEmpty(); i++) {
+            double x = in.readDouble();
+            double y = in.readDouble();
+            Point2D p = new Point2D(x, y);
+            if(kdTree.contains(p)) count++;
+        }
+
+        System.out.println("kdTree contains check: " + count);
 
         count = 0;
         for(Point2D p: kdTree.range(rect)){
